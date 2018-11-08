4a14123187ec01adefeea9f207fca7383e0d64b8
mrstevenaweiss <weiss.steven@gmail.com>
2018-11-08 14:54:36 -0500

    finished chp 10 in lynda js

--- Lynda/js-fundamentals/Exercise Files/07_03/JS/script.js
+++ Lynda/js-fundamentals/Exercise Files/07_03/JS/script.js
@@ -1,3 +1,18 @@
 
 const CTA = document.querySelector('.cta a')
 const ALERT = document.querySelector('#booking-alert')
+
+console.log(CTA);
+
+CTA.classList.remove("hide")
+ALERT.classList.add("hide")
+
+function reveal(e, current){
+  e.preventDefault();
+
+  current.innerHTML == "Book Now!" ? CTA.innerHTML = "Oooops!" : CTA.innerHTML = "Book Now!"
+
+  ALERT.classList.toggle("hide")
+}
+
+CTA.addEventListener("click", function(e) { reveal(e,this); }, false);
